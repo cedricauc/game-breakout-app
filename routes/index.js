ed955ce9dc9b2319e9ffe33dada9c6d337c9cfb3
Cedric Auclair <39864243+cedricauc@users.noreply.github.com>
2022-10-31 16:28:43 +0100

    edit index.js

--- routes/index.js
+++ routes/index.js
@@ -17,14 +17,29 @@ router.get('/game', passport.authenticate('session'), async function(req, res) {
     flag = false
     msg = "Vous avez joué le jeu gratuit"
   } else msg = "Vous avez 1 jeu gratuit"
-  res.render('game', {user: req.user.pseudo, flag, msg});
+  res.render('game', {user: req.user.pseudo, flag, msg, level: req.user.level, lives: req.user.lives,});
 });
 
-router.post('/shop', passport.authenticate('session'),  function(req, res) {
+router.post('/shop', passport.authenticate('session'), async function(req, res) {
   res.render('shop', {user: req.user.pseudo});
 });
 
-router.post('/home', passport.authenticate('session'),  function(req, res) {
+router.get('/shop', passport.authenticate('session'), async function(req, res) {
+  res.render('shop', {user: req.user.pseudo});
+});
+
+router.get('/trophy', passport.authenticate('session'), async function(req, res) {
+  // Modify DB
+  //console.log('route get ', req.user.pseudo);
+  let today = new Date().toDateString()
+  let flag = true
+  if(today == req.user.date) {
+    flag = false
+  }
+  res.render('trophy', {user: req.user.pseudo, level: req.user.level, lives: req.user.lives,  flag: flag});
+});
+
+router.post('/home', passport.authenticate('session'), async function(req, res) {
   res.render('dashboard', {user: req.user.pseudo});
 });
 
